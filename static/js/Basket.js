d13963893573c3ae8db0da9f3533797e47830dde
Kamil Drzazga <drzazga888@gmail.com>
2015-05-16 19:56:54 +0200

    koszyk i ostatnio przegladane juz dziala

--- static/js/Basket.js
+++ static/js/Basket.js
@@ -1,12 +1,12 @@
 Basket = function(localStorageName) {
 	this.localStorageName = localStorageName;
-}
+	if (!localStorage[localStorageName])
+		localStorage[localStorageName] = JSON.stringify([]);
+};
 
 Basket.prototype.add = function(book) {
 	var books = this.get();
-	if (books == null)
-		books = [];
-	else if (this.indexOf(book.id, books) !== undefined) {
+	if (this.indexOf(book.id, books) !== undefined) {
 		console.warn("Nie można dodać książki która już istnieje");
 		return;
 	}
@@ -15,11 +15,7 @@ Basket.prototype.add = function(book) {
 };
 
 Basket.prototype.get = function() {
-	var books = localStorage.getItem(this.localStorageName);
-	if (books)
-		return JSON.parse(books);
-	else
-		return null;
+	return JSON.parse(localStorage.getItem(this.localStorageName));
 };
 
 Basket.prototype.remove = function(bookID) {
@@ -39,4 +35,47 @@ Basket.prototype.indexOf = function(bookID, books) {
 			return i;
 	}
 	return undefined;
+};
+
+Basket.prototype.show = function() {
+	var books = basket.get();
+	var form = $(".basket");
+	var basketInfo = form.children(".dropdown-toggle");
+	basketInfo.children(".basket-item-count").children(".count").text(books ? books.length : 0);
+	var bookContainer = form.children(".dropdown-menu");
+	bookContainer.children().not(".checkout").remove();
+	var totalPrice = Number(0);
+	if (books.length > 0) {
+		for (var i = books.length - 1; i >= 0; --i) {
+			totalPrice += Number(books[i].price.replace(",", "."));
+			var toPrepend = this.stringify(books[i]);
+			bookContainer.prepend(toPrepend);
+		}
+	}
+	else
+		bookContainer.prepend('<li><p class="basket-item">Brak książek w koszyku</p></li>');
+	totalPrice = totalPrice.toFixed(2);
+	basketInfo.children(".total-price-basket").children(".total-price").children(".value").text(totalPrice);
+};
+
+Basket.prototype.stringify = function(book) {
+	return '\
+		<li>\
+			<div class="basket-item book-container" data-book-id="' + book.id + '">\
+				<div class="row">\
+					<div class="col-xs-4 col-sm-4 text-center fit-me">\
+						<div class="thumb fit-me">\
+							<img alt="" src="/static/books/' + book.id + '.jpg" />\
+						</div>\
+					</div>\
+					<div class="col-xs-8 col-sm-8">\
+						<div class="title book-title">' + book.title + '</div>\
+						<div class="author book-author">' + book.author + '</div>\
+						<div class="price book-price">' + book.price + ' PLN</div>\
+					</div>\
+				</div>\
+				<a class="close-btn" href="javascript:void(0);" onclick="Book.remove(event, basket)"></a>\
+			</div>\
+		</li>\
+	';
 };
--- static/js/LocalStorage.js
+++ static/js/LocalStorage.js
@@ -4,97 +4,10 @@ var recentlyViewed;
 $(document).ready(function(){
 	basket = new Basket("basket");
 	recentlyViewed = new RecentlyViewed("recently-viewed");
-	showBooksInBasket();
+	basket.show();
+	recentlyViewed.show();
 });
 
 $('.dropdown-menu').click(function(e) {
     e.stopPropagation();
 });
-
-// BASKET
-
-function addBookFromListToBasket(button) {
-	button = $(button);
-	var form = button.parent().parent().parent();
-	var book = new Book();
-	book.id = form.children(".image").children("img").attr("src").split("/")[3].split(".")[0];
-	book.title = form.children(".body").children(".title").children("a").text();
-	book.author = form.children(".body").children(".brand").text();
-	book.price = form.children(".prices").children(".price-current").text().split(" ")[0];
-	basket.add(book);
-	showBooksInBasket();
-}
-
-function removeBookFromBasket(button) {
-	var thumb = $(button).parent().children(".row").find(".thumb").children("img");
-	var id = thumb.attr("src").split("/")[3].split(".")[0];
-	basket.remove(id);
-	showBooksInBasket();
-}
-
-function showBooksInBasket() {
-	var books = basket.get();
-	var form = $(".basket");
-	var basketInfo = form.children(".dropdown-toggle");
-	basketInfo.children(".basket-item-count").children(".count").text(books ? books.length : 0);
-	var bookContainer = form.children(".dropdown-menu");
-	bookContainer.children().not(".checkout").remove();
-	var totalPrice = Number(0);
-	if (books && books.length > 0) {
-		for (var i = books.length - 1; i >= 0; --i) {
-			totalPrice += Number(books[i].price.replace(",", "."));
-			var toPrepend = getBasketElementString(books[i]);
-			bookContainer.prepend(toPrepend);
-		}
-	}
-	else
-	{
-		console.log("0");
-		bookContainer.prepend('<li><p class="basket-item">Brak książek w koszyku</p></li>');
-	}
-	totalPrice = totalPrice.toFixed(2);
-	basketInfo.children(".total-price-basket").children(".total-price").children(".value").text(totalPrice);
-}
-
-function getBasketElementString(book) {
-	return '\
-		<li>\
-			<div class="basket-item">\
-				<div class="row">\
-					<div class="col-xs-4 col-sm-4 text-center fit-me">\
-						<div class="thumb fit-me">\
-							<img alt="" src="/static/books/' + book.id + '.jpg" />\
-						</div>\
-					</div>\
-					<div class="col-xs-8 col-sm-8">\
-						<div class="title">' + book.title + '</div>\
-						<div class="author">' + book.author + '</div>\
-						<div class="price">' + book.price + ' PLN</div>\
-					</div>\
-				</div>\
-				<a class="close-btn" href="javascript:void(0);" onclick="removeBookFromBasket(this)"></a>\
-			</div>\
-		</li>\
-	';
-}
-
-// RECENTLY VIEWED
-
-function addBookToRecentlyViewed() {
-	var recentlyViewedForm = $("#recently-viewed-add-form");
-	if (!recentlyViewedForm[0].checkValidity()) {
-		console.error(recentlyViewedForm[0].validationMessage);
-		return;
-	}
-	var book = new Book();
-	book.id = recentlyViewedForm.children(".id").val();
-	book.title = recentlyViewedForm.children(".title").val();
-	book.author = recentlyViewedForm.children(".author").val();
-	book.price = recentlyViewedForm.children(".price").val();
-	recentlyViewed.add(book);
-}
-
-function showBooksInRecentlyViewed() {
-	var books = recentlyViewed.get();
-	console.log(books);
-}
